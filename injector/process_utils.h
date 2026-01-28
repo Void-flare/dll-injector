1c4e60bb591d84aea19cf9f9d8d4ca11daf36a80
Void Flare <specter.business.specter@gmail.com>
2026-01-28 15:51:07 +0100

    .

--- injector/process_utils.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <string>
-#include <windows.h>
-
-DWORD FindProcessIdByName(const std::wstring& processName);
--- injector/test_dll.cpp
+++
@@ -1,9 +0,0 @@
-#include <windows.h>
-
-extern "C" __declspec(dllexport)
-BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
-    if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
-        MessageBoxA(NULL, "Injection Successful!", "Test DLL", MB_OK | MB_ICONINFORMATION);
-    }
-    return TRUE;
-}
